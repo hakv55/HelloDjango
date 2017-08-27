65c69d3165898dbed0b3d28f11774abeabac147f
hakv <hakv@microsoft.com>
2017-08-27 22:34:38 +0530

    new project setup with model definition

---
+++ visitsapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StorevisitsConfig(AppConfig):
+    name = 'visitsapi'
---
+++ visitsapi/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Visit(models.Model):
+
+    user_id = models.CharField(max_length=2)
+    latitude = models.DecimalField(max_digits=5, decimal_places=6)
+    longitude = models.DecimalField(max_digits=5, decimal_places=6)
+    rate = models.DecimalField(max_digits=8, decimal_places=2)
+
