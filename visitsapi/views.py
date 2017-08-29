d14204794478af196645a5bf5d5e7fab9ccbb57c
hakv <hakv@microsoft.com>
2017-08-29 09:47:50 +0530

    Addition : GET Method for retrieving all Visits Post method for adding a Visit

--- visitsapi/views.py
+++ visitsapi/views.py
@@ -4,21 +4,33 @@ from django.http import JsonResponse
 from rest_framework import status
 from django.core import serializers
 import json
+from django.views.decorators.csrf import csrf_exempt
+from .models import Visit
 # Create your views here.
 
 
+@csrf_exempt
+def visits(request):
 
-def add_visit(request):
-    visit_form = VisitsForm(request.post)
-    is_valid = visit_form.is_valid()
-    if not is_valid:
-        response = JsonResponse({"errors": visit_form.errors.get_json_data()})
-        response.status_code = status.HTTP_400_BAD_REQUEST
-    else:
-        visit = visit_form.save()
-        data = serializers.serialize("json", [visit, ])
-        response = JsonResponse(json.loads(data))
-        response.status_code = status.HTTP_201_CREATED
-    return response
+    if request.method == "POST":
+        visit_form = VisitsForm(request.POST)
+        is_valid = visit_form.is_valid()
+        if not is_valid:
+            response = JsonResponse({"errors": visit_form.errors})
+            response.status_code = status.HTTP_400_BAD_REQUEST
+        else:
+            visit = visit_form.save()
+            data = json.loads(serializers.serialize("json", [visit, ]))[0] # Model.objects?
+            response_data = {'id': data['pk']}
+            response_data.update(data['fields'])
+            response = JsonResponse(response_data)
+            response.status_code = status.HTTP_201_CREATED
+        return response
+
+    elif request.method == "GET":
+        data = json.loads(serializers.serialize("json", Visit.objects.all()))  # Model.objects?
+        response = JsonResponse(data, safe=False)
+        response.status_code = status.HTTP_200_OK
+        return response
 
 
